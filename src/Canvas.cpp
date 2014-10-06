00b84b9d19c81ce5de35f86490dfb8803b930e34
rfrapp <rfrapp@gmail.com>
2014-10-06 09:42:55 -0500

    - Stuff

--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -26,11 +26,11 @@ bool Canvas::load_media()
 
 void Canvas::init_controls()
 {
-	// Button *b = new Button(renderer, 30, 30, 100, 100, "", 
-	// 	        "images/SVG-icons.png", 4, 6, 30, 30);
-	TextBox *t = new TextBox(renderer, font, 200, h - 20, 10, 10,
-		                                         true, false, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 
-		                                         0xBB, 0xBB, 0xBB);
+	Button *b = new Button(renderer, 30, 30, 100, 100, "", 
+		        "images/icons.png", 4, 6, 30, 30);
+	// TextBox *t = new TextBox(renderer, font, 200, h - 20, 10, 10,
+	// 	                                         true, false, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 
+	// 	                                         0xBB, 0xBB, 0xBB);
 
-	controls.push_back(t);
+	//controls.push_back(t);
 }
